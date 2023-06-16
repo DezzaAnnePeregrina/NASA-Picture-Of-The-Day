4e829f753a92542caa9d82e511246885e4e8bd3e
DezzaAnnePeregrina <peregrinadezzaanne@gmail.com>
2023-06-16 17:10:56 +0800

    changed styles

--- js/main.js
+++ js/main.js
@@ -1,5 +1,28 @@
 document.querySelector('button').addEventListener('click', getFetch)
 
+const url = `https://api.nasa.gov/planetary/apod?api_key=04UeUb6kPFVEjJ9kPfYltWWpgJ71q5R6lb7f8rSl`
+  //04UeUb6kPFVEjJ9kPfYltWWpgJ71q5R6lb7f8rSl
+  fetch(url)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+        console.log(data)
+
+        document.querySelector('h1').innerText = data.title
+        if(data.media_type === "video"){
+          document.querySelector('.img__media').style.display = 'none'
+          document.querySelector('iframe').src = data.url
+        } else if(data.media_type === "image"){
+          document.querySelector('iframe').style.display = 'none'
+          document.querySelector('.img__media').src = data.hdurl
+      }
+      document.querySelector('h3').innerText = data.explanation
+
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
+
+
 function getFetch(){
   const choice = document.querySelector('input').value
   const url = `https://api.nasa.gov/planetary/apod?api_key=04UeUb6kPFVEjJ9kPfYltWWpgJ71q5R6lb7f8rSl&date=${choice}`
@@ -9,13 +32,13 @@ function getFetch(){
       .then(data => {
         console.log(data)
 
-        document.querySelector('h2').innerText = data.title
+        document.querySelector('h1').innerText = data.title
         if(data.media_type === "video"){
-          document.querySelector('img').style.display = 'none'
+          document.querySelector('.img__media').style.display = 'none'
           document.querySelector('iframe').src = data.url
         } else if(data.media_type === "image"){
           document.querySelector('iframe').style.display = 'none'
-          document.querySelector('img').src = data.hdurl
+          document.querySelector('.img__media').src = data.hdurl
       }
       document.querySelector('h3').innerText = data.explanation
       })
